61c833198f1b8d0838fe44af0bff502315eeac9e
Jack Ellis <jackellis1504@gmail.com>
2026-06-16 11:24:25 +0100

    update how migrations and models work
     M app/Models/User.php
     M database/migrations/0001_01_01_000000_create_users_table.php
     M stubs/migration.create.stub
     M stubs/model.stub

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,9 @@ namespace App\Models;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -15,7 +17,10 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory;
+    use Notifiable;
+    use HasUuids;
+    use SoftDeletes;
 
     /**
      * Get the attributes that should be cast.
